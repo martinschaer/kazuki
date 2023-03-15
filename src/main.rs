df780a1b04713e8b3a6864c4209a4ccfa8d15ef1
Martin Schaer <martin@schaerweb.com>
2023-03-15 06:39:00 -0600

    add three-d

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,67 @@
-use beryllium::{
-    events::Event,
-    init::InitFlags,
-    video::{CreateWinArgs, GlProfile},
-    Sdl,
-};
+#[cfg(not(target_arch = "wasm32"))]
+#[tokio::main]
+async fn main() {
+    run().await;
+}
 
-fn main() {
-    // Initializes SDL2
-    let sdl = Sdl::init(InitFlags::EVERYTHING);
+use three_d::*;
 
-    // This part asks for an ES 3.1 context "just for fun", because that's what
-    // works best between Windows and also Raspberry Pi. Mac doesn't support ES
-    // contexts, but this is just a demo so for Mac we'll skip any configuration
-    // at all and just get some "don't care" GL context.
-    #[cfg(not(target_os = "macos"))]
-    {
-        sdl.set_gl_profile(GlProfile::ES).unwrap();
-        sdl.set_gl_context_major_version(3).unwrap();
-        sdl.set_gl_context_minor_version(1).unwrap();
-    }
+pub async fn run() {
+    let window = Window::new(WindowSettings {
+        title: "Kazuki".to_string(),
+        max_size: Some((1280, 720)),
+        ..Default::default()
+    })
+    .unwrap();
 
-    // Makes the window with a GL Context.
-    let win = sdl
-        .create_gl_window(CreateWinArgs {
-            title: "Example GL Window",
-            ..Default::default()
-        })
-        .unwrap();
-    println!("GL window size: {:?}", win.get_window_size());
-    println!("GL drawable size: {:?}", win.get_drawable_size());
-    println!(
-        "GL_KHR_debug supported: {}",
-        win.supports_extension("GL_KHR_debug")
+    let context = window.gl();
+
+    let viewport = window.viewport();
+    // let h = viewport.height;
+    // let w = viewport.width;
+    let mut camera = Camera::new_orthographic(
+        viewport,
+        vec3(0.0, 0.0, 2.0),
+        vec3(0.0, 0.0, 0.0),
+        vec3(0.0, 1.0, 0.0),
+        0.5,
+        0.1,
+        10.0,
     );
 
-    let mut controllers = Vec::new();
+    let positions = vec![
+        vec3(0.5, -0.5, 0.0),
+        vec3(-0.5, -0.5, 0.0),
+        vec3(0.0, 0.5, 0.0),
+    ];
+    let colors = vec![
+        Color::new(255, 0, 0, 255),
+        Color::new(0, 255, 0, 255),
+        Color::new(0, 0, 255, 255),
+    ];
+    let cpu_mesh = CpuMesh {
+        positions: Positions::F32(positions),
+        colors: Some(colors),
+        ..Default::default()
+    };
 
-    // program "main loop".
-    'the_loop: loop {
-        // Process events from this frame.
-        #[allow(clippy::never_loop)]
-        while let Some((event, _timestamp)) = sdl.poll_events() {
+    let mut model = Gm::new(Mesh::new(&context, &cpu_mesh), ColorMaterial::default());
+    model.set_animation(|time| Mat4::from_angle_y(radians(time * 0.005)));
+
+    window.render_loop(move |frame_input| {
+        camera.set_viewport(frame_input.viewport);
+        model.animate(frame_input.accumulated_time as f32);
+        for event in frame_input.events.iter() {
             match event {
-                Event::Quit => break 'the_loop,
-                Event::ControllerAdded { index } => match sdl.open_game_controller(index) {
-                    Ok(controller) => {
-                        println!(
-                            "Opened `{name}` (type: {type_:?})",
-                            name = controller.get_name(),
-                            type_ = controller.get_type()
-                        );
-                        controllers.push(controller);
-                    }
-                    Err(msg) => println!("Couldn't open {index}: {msg:?}"),
-                },
-                Event::JoystickAxis { .. }
-                | Event::ControllerAxis { .. }
-                | Event::MouseMotion { .. } => (),
-                _ => println!("{event:?}"),
+                _ => {}
             }
         }
 
-        // TODO: post-events drawing
+        frame_input
+            .screen()
+            .clear(ClearState::color_and_depth(0.0, 0.0, 0.0, 1.0, 1.0))
+            .render(&camera, &model, &[]);
 
-        // TODO: swap buffers.
-    }
-
-    // All the cleanup is handled by the various drop impls.
+        FrameOutput::default()
+    })
 }
