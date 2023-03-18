1cbfba7a7350a0e09c389876390adb30cdddab21
Martin Schaer <martin@schaerweb.com>
2023-03-18 16:43:57 -0600

    backup three-d version

---
+++ apps/three-d/src/lib.rs
@@ -0,0 +1,25 @@
+#[cfg(target_arch = "wasm32")]
+mod services;
+
+// Entry point for wasm
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(start)]
+pub async fn start() -> Result<(), JsValue> {
+    console_log::init_with_level(log::Level::Debug).expect("error initializing log");
+
+    use log::info;
+    info!("Logging works!!");
+
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    services::game::run().await;
+    Ok(())
+}
