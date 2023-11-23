bf80cd0b3d94b1501113ec32974e2bc9ac1a7124
Martin Schaer <martin@schaerweb.com>
2023-11-23 23:13:32 +0000

    add body and connect with upright

--- apps/game/examples/src/joints.rs
+++ apps/game/examples/src/joints.rs
@@ -3,7 +3,8 @@ use std::f32::consts::PI;
 use bevy::app::{App, Plugin};
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::{
-    Collider, GenericJoint, GenericJointBuilder, ImpulseJoint, JointAxesMask, JointAxis, RigidBody,
+    Collider, ColliderMassProperties, GenericJoint, GenericJointBuilder, ImpulseJoint,
+    JointAxesMask, JointAxis, MultibodyJoint, RigidBody,
 };
 
 use super::JointsPlugin;
@@ -14,7 +15,7 @@ impl Plugin for JointsPlugin {
     }
 }
 
-fn make_wheel_joint(
+fn make_joint(
     locked_axes: JointAxesMask,
     parent_axis: Vec3,
     child_axis: Vec3,
@@ -24,40 +25,58 @@ fn make_wheel_joint(
     motor: (f32, f32, f32, f32),
 ) -> GenericJoint {
     let unlocked_axis = if (JointAxesMask::all() - locked_axes).contains(JointAxesMask::ANG_X) {
-        JointAxis::AngX
+        Some(JointAxis::AngX)
     } else if (JointAxesMask::all() - locked_axes).contains(JointAxesMask::ANG_Y) {
-        JointAxis::AngY
+        Some(JointAxis::AngY)
+    } else if (JointAxesMask::all() - locked_axes).contains(JointAxesMask::ANG_Z) {
+        Some(JointAxis::AngZ)
     } else {
-        JointAxis::AngZ
+        None
     };
-    GenericJointBuilder::new(locked_axes)
+    let mut builder = GenericJointBuilder::new(locked_axes)
         .local_axis2(parent_axis)
         .local_axis1(child_axis)
         .local_anchor2(parent_anchor)
-        .local_anchor1(child_anchor)
-        .set_motor(unlocked_axis, motor.0, motor.1, motor.2, motor.3)
-        .build()
+        .local_anchor1(child_anchor);
+    if let Some(u_a) = unlocked_axis {
+        builder = builder.set_motor(u_a, motor.0, motor.1, motor.2, motor.3);
+    }
+    builder.build()
 }
 
 fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
     let mesh = meshes.add(Mesh::from(shape::Cube { size: 1. }));
+    let upright_mesh = meshes.add(Mesh::from(shape::Cube { size: 0.4 }));
     let wheel_mesh = meshes.add(Mesh::from(shape::Cylinder {
         radius: 0.5,
         height: 1.,
         ..default()
     }));
-    let cube = commands
-        .spawn((PbrBundle {
+
+    let body = commands
+        .spawn(PbrBundle {
             mesh: mesh.clone(),
-            transform: Transform::from_xyz(0., 1., 0.),
+            transform: Transform::from_xyz(-1.2, 1.6, 0.),
             ..default()
-        },))
+        })
+        .insert(Name::new("Body"))
         .insert(RigidBody::KinematicPositionBased)
         .insert(Collider::cuboid(0.5, 0.5, 0.5))
         .id();
 
+    let upright = commands
+        .spawn(PbrBundle {
+            mesh: upright_mesh.clone(),
+            transform: Transform::from_xyz(0., 1., 0.),
+            ..default()
+        })
+        .insert(RigidBody::Dynamic)
+        .insert(Collider::cuboid(0.2, 0.2, 0.2))
+        .insert(ColliderMassProperties::Density(4.))
+        .id();
+
     let wheel = commands
-        .spawn((PbrBundle {
+        .spawn(PbrBundle {
             mesh: wheel_mesh.clone(),
             transform: Transform {
                 translation: Vec3::new(1.2, 1., 0.),
@@ -65,13 +84,13 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
                 ..default()
             },
             ..default()
-        },))
+        })
         .insert(RigidBody::Dynamic)
         .insert(Collider::cylinder(0.5, 0.5))
         .id();
 
-    // Joint
-    let wheel_joint = make_wheel_joint(
+    // Wheel - Upright Joint
+    let wheel_joint = make_joint(
         JointAxesMask::X
             | JointAxesMask::Y
             | JointAxesMask::Z
@@ -81,10 +100,27 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
         Vec3::X,
         Vec3::new(0., 0.6, 0.),
         Vec3::new(0.6, 0., 0.),
-        (0., 1e2, 1e6, 1e3),
+        (0., 5., 0., 0.),
     );
 
     commands
         .entity(wheel)
-        .insert(ImpulseJoint::new(cube, wheel_joint));
+        // with this it the motor doesn't work
+        .insert(MultibodyJoint::new(upright, wheel_joint));
+    // with this the motor works
+    // .insert(ImpulseJoint::new(upright, wheel_joint));
+
+    // Upright - Body Joint
+    let upright_joint = make_joint(
+        JointAxesMask::Y | JointAxesMask::Z | JointAxesMask::ANG_Y | JointAxesMask::ANG_Z,
+        Vec3::Y,
+        Vec3::Y,
+        Vec3::new(-0.6, 0., 0.),
+        Vec3::new(0.6, 0., 0.),
+        (0., 0., 0., 0.),
+    );
+
+    commands
+        .entity(upright)
+        .insert(ImpulseJoint::new(body, upright_joint));
 }
