a4bb6fc640e0286801a451877b24be5a650c3a23
Martin Schaer <martin@schaerweb.com>
2023-12-22 12:01:37 -0600

    make world inspector a feature

--- apps/game/src/game.rs
+++ apps/game/src/game.rs
@@ -1,6 +1,12 @@
-use bevy::window::WindowResized;
-use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, prelude::*, window::PresentMode};
+use bevy::{
+    core::TaskPoolThreadAssignmentPolicy, prelude::*, tasks::available_parallelism,
+    window::PresentMode,
+};
+use bevy::{diagnostic::FrameTimeDiagnosticsPlugin, window::WindowResized};
+
+#[cfg(feature = "inspector")]
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
 use bevy_rapier3d::{
     prelude::{NoUserData, RapierPhysicsPlugin},
     render::RapierDebugRenderPlugin,
@@ -9,35 +15,53 @@ use bevy_rapier3d::{
 use crate::plugins::{CameraType, CarPlugin, ControlsPlugin, CubesPlugin, MainScenePlugin};
 
 pub fn run() {
-    App::new()
-        .add_plugins(
-            DefaultPlugins
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        title: "Kazuki!".into(),
-                        resolution: (640., 480.).into(),
-                        present_mode: PresentMode::AutoVsync,
-                        canvas: Some("main canvas".into()),
-                        fit_canvas_to_parent: true,
-                        prevent_default_event_handling: false,
-                        ..default()
-                    }),
+    let mut app = App::new();
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Kazuki!".into(),
+                    resolution: (640., 480.).into(),
+                    present_mode: PresentMode::AutoVsync,
+                    canvas: Some("main canvas".into()),
+                    fit_canvas_to_parent: true,
+                    prevent_default_event_handling: false,
                     ..default()
-                })
-                .set(AssetPlugin::default()),
-        )
-        .add_plugins(FrameTimeDiagnosticsPlugin)
-        .add_plugins(MainScenePlugin {
-            camera_type: CameraType::Follow,
-        })
-        .add_plugins(CarPlugin)
-        .add_plugins(ControlsPlugin)
-        .add_plugins(CubesPlugin)
-        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
-        .add_plugins(RapierDebugRenderPlugin::default())
-        .add_plugins(WorldInspectorPlugin::new())
-        .add_systems(Update, on_resize_system)
-        .run();
+                }),
+                ..default()
+            })
+            .set(AssetPlugin::default())
+            .set(TaskPoolPlugin {
+                task_pool_options: TaskPoolOptions {
+                    compute: TaskPoolThreadAssignmentPolicy {
+                        // set the minimum # of compute threads
+                        // to the total number of available threads
+                        min_threads: available_parallelism(),
+                        max_threads: std::usize::MAX, // unlimited max threads
+                        percent: 1.0,                 // this value is irrelevant in this case
+                    },
+                    // keep the defaults for everything else
+                    ..default()
+                },
+            }),
+    )
+    .add_plugins(MainScenePlugin {
+        camera_type: CameraType::Follow,
+    })
+    .add_plugins(CarPlugin)
+    .add_plugins(ControlsPlugin)
+    .add_plugins(CubesPlugin)
+    .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+    .add_plugins(FrameTimeDiagnosticsPlugin)
+    .add_plugins(RapierDebugRenderPlugin::default())
+    .add_systems(Update, on_resize_system);
+
+    #[cfg(feature = "inspector")]
+    {
+        app.add_plugins(WorldInspectorPlugin::new());
+    }
+
+    app.run();
 }
 
 fn on_resize_system(mut resize_reader: EventReader<WindowResized>) {
