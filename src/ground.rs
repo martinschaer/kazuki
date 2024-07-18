04084ffeb5423a6a0267c694aa3183b682e08938
Martin Schaer <martin@schaerweb.com>
2024-07-18 23:48:55 +0100

    animated ground

--- src/ground.rs
+++ src/ground.rs
@@ -1,27 +1,87 @@
 use bevy::{
     prelude::*,
     render::{
-        mesh::{Indices, PrimitiveTopology},
+        mesh::{Indices, PrimitiveTopology, VertexAttributeValues},
         render_asset::RenderAssetUsages,
     },
 };
+use noise::{core::perlin::perlin_2d, permutationtable::PermutationTable, Vector2};
 
-pub fn build_ground() -> Mesh {
+pub fn build_ground(width: u32, height: u32, cols: u32, rows: u32) -> Mesh {
+    let middle_x = width as f32 / 2.0;
+    let middle_z = height as f32 / 2.0;
+    let tri_w = width as f32 / cols as f32;
+    let tri_h = height as f32 / rows as f32;
+
+    // vertices
+    let vertices = (0..rows + 1)
+        .flat_map(|row| {
+            (0..cols + 1).map(move |col| {
+                let x = col as f32 * tri_w;
+                let z = row as f32 * tri_h;
+                (x - middle_x, z - middle_z)
+            })
+        })
+        .map(|(x, z)| [x, 0.0, z])
+        .collect::<Vec<[f32; 3]>>();
+    assert_eq!(
+        vertices.len(),
+        ((cols + 1) * (rows + 1)) as usize,
+        "vertices.len()"
+    );
+
+    // indices
+    let indices = (0..rows)
+        .flat_map(|row| {
+            (0..cols).flat_map(move |col| {
+                vec![
+                    (row + 1) * (cols + 1) + col + 1,
+                    row * (cols + 1) + col + 1,
+                    row * (cols + 1) + col,
+                    (row + 1) * (cols + 1) + col,
+                    (row + 1) * (cols + 1) + col + 1,
+                    row * (cols + 1) + col,
+                ]
+            })
+        })
+        .collect::<Vec<u32>>();
+    assert_eq!(indices.len(), (cols * rows * 6) as usize, "indices.len()");
+
+    // normals and uvs
+    let normals = vec![[0.0, 1.0, 0.0]; vertices.len()];
+    let uvs = (0..rows + 1)
+        .flat_map(|row| {
+            (0..cols + 1).map(move |col| {
+                let u = col as f32 / cols as f32;
+                let v = row as f32 / rows as f32;
+                (u, v)
+            })
+        })
+        .map(|(u, v)| [u, v])
+        .collect::<Vec<[f32; 2]>>();
+    assert_eq!(uvs.len(), vertices.len(), "uvs.len()");
+
+    // mesh
     Mesh::new(
         PrimitiveTopology::TriangleList,
         RenderAssetUsages::default(),
     )
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        vec![[-0.5, 0.0, -0.5], [0.5, 0.0, -0.5], [-0.5, 0.0, 0.5]],
-    )
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_NORMAL,
-        vec![[0.0, 1.0, 0.0], [0.0, 1.0, 0.0], [0.0, 1.0, 0.0]],
-    )
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        vec![[0.0, 0.0], [1.0, 0.0], [0.0, 1.0]],
-    )
-    .with_inserted_indices(Indices::U32(vec![0, 1, 2]))
+    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
+    .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, normals)
+    .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, uvs)
+    .with_inserted_indices(Indices::U32(indices))
+}
+
+pub fn update_ground(mesh: &mut Mesh, t: f32, perm_table: &PermutationTable) {
+    let vertices = mesh.attribute_mut(Mesh::ATTRIBUTE_POSITION);
+    if let Some(vertices) = vertices {
+        if let VertexAttributeValues::Float32x3(v) = vertices {
+            for vertex in v.iter_mut() {
+                let x = vertex[0];
+                let z = vertex[2];
+                let noise = perlin_2d(Vector2::new((x + t * 0.5) as f64, z as f64), perm_table);
+                *vertex = [x, noise as f32 * 0.2, z];
+            }
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use bevy::{
-    color::palettes::css::ORANGE_RED, input::common_conditions::input_toggle_active,
-    pbr::CascadeShadowConfigBuilder, prelude::*,
+    color::palettes::css::ORANGE_RED,
+    input::common_conditions::input_toggle_active,
+    pbr::CascadeShadowConfigBuilder,
+    prelude::*,
+    render::camera::{Exposure, PhysicalCameraParameters},
 };
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use noise::permutationtable::PermutationTable;
 use std::f32::consts::PI;
 
-use ground::build_ground;
+use ground::{build_ground, update_ground};
 
 mod ground;
+
+#[derive(Component)]
+struct Ground;
+
+#[derive(Resource)]
+struct GroundParams {
+    permutation_table: PermutationTable,
+}
+
+#[derive(Resource, Default, Deref, DerefMut)]
+struct Parameters(PhysicalCameraParameters);
 
 #[bevy_main]
 fn main() {
@@ -17,7 +32,14 @@ fn main() {
             WorldInspectorPlugin::default().run_if(input_toggle_active(true, KeyCode::Escape)),
         )
         // .add_plugins((DefaultPlugins, DevPlugins))
+        .insert_resource(Parameters(PhysicalCameraParameters {
+            aperture_f_stops: 1.0,
+            shutter_speed_s: 1.0 / 125.0,
+            sensitivity_iso: 100.0,
+            sensor_height: 0.01866,
+        }))
         .add_systems(Startup, setup)
+        .add_systems(Update, system_update_ground)
         .run();
 }
 
@@ -25,23 +47,28 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    parameters: Res<Parameters>,
 ) {
-    // ground plane
-    commands.spawn(PbrBundle {
-        // mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
-        mesh: meshes.add(Plane3d::new(Vec3::Y, Vec2::new(5., 5.))),
-        material: materials.add(Color::srgb_u8(124, 144, 255)),
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        ..default()
-    });
+    // permutation table
+    let permutation_table = PermutationTable::new(0);
+    let ground_params = GroundParams { permutation_table };
+    commands.insert_resource(ground_params);
 
     // ground
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(build_ground()),
-        material: materials.add(Color::srgb_u8(0, 125, 125)),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(build_ground(10, 10, 10, 10)),
+            // material: materials.add(Color::srgb_u8(0, 125, 125)),
+            material: materials.add(StandardMaterial {
+                base_color: Color::srgb_u8(0, 125, 125),
+                perceptual_roughness: 1.0,
+                ..default()
+            }),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        },
+        Ground,
+    ));
 
     // ambient light
     commands.insert_resource(AmbientLight {
@@ -53,7 +80,7 @@ fn setup(
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
             illuminance: light_consts::lux::OVERCAST_DAY,
-            // shadows_enabled: true,
+            shadows_enabled: true,
             ..default()
         },
         transform: Transform {
@@ -75,7 +102,23 @@ fn setup(
 
     // camera
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(5.0, 2.5, 0.).looking_at(Vec3::ZERO, Vec3::Y),
+        exposure: Exposure::from_physical_camera(**parameters),
         ..default()
     });
+}
+
+fn system_update_ground(
+    time: Res<Time>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mesh_query: Query<&Handle<Mesh>, With<Ground>>,
+    res_ground_params: Res<GroundParams>,
+) {
+    let mesh_handle = mesh_query.get_single().expect("Query not successful");
+    let mesh = meshes.get_mut(mesh_handle).unwrap();
+    update_ground(
+        mesh,
+        time.elapsed_seconds(),
+        &res_ground_params.permutation_table,
+    );
 }
